05e2274371a427323af387cdcdcbe67dbbf73059
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-11-26 19:30:30 -0600

    Changes?

--- PacketTecServer/src/com/tec/resources/Application.java
+++ PacketTecServer/src/com/tec/resources/Application.java
@@ -4,11 +4,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 
+import Mensajes.RecursosMensajes;
 import Register.RegisterResource;
 
-public class Application extends ResourceConfig  {
+public class Application extends ResourceConfig {
 	public Application() {
 		register(RegisterResource.class);
 		register(JacksonFeatures.class);
+		register(RecursosMensajes.class);
 	}
 }
