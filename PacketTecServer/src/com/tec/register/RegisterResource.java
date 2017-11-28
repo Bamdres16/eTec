cc9e8383a286ef0d20fdbcf78615158d25a61e61
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-11-27 18:53:55 -0600

    Register detected

--- PacketTecServer/src/com/tec/register/RegisterResource.java
+++ PacketTecServer/src/com/tec/register/RegisterResource.java
@@ -3,6 +3,7 @@ package com.tec.register;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -17,20 +18,38 @@ import org.json.simple.parser.ParseException;
 
 @Path("/register")
 public class RegisterResource {
-	private static List<RegisterEntity> data = new ArrayList<>();
+	private static  JSONArray data = new JSONArray(); 
 	private static JSONObject results = new JSONObject();
-
+	public RegisterResource() {
+		// TODO Auto-generated constructor stub
+		results.put("results",data);
+		}
 	@SuppressWarnings("unchecked")
 	@POST
 	@Produces("application/json")
 	@Consumes("application/json")
 	public Response registrar(RegisterEntity registro) {
+		JSONArray compare = (JSONArray) results.get("results");
+		JSONObject reg = convertir(registro);
+		if (compare.contains(reg)) {
+			return Response.ok("Exist").build();
+		}
+		else {
+			
+			data.add(reg);
+			results.put("results", data);
+			return Response.ok("Sucess").build();
+		}
+	}
+	@SuppressWarnings("unchecked")
+	private JSONObject convertir (RegisterEntity registro) {
+		JSONObject reg = new JSONObject();
+		reg.put("name", registro.getName());
+		reg.put("username", registro.getUsername());	
+		reg.put("password", registro.getPassword());
+		reg.put("email", registro.getEmail());
+		return reg;
 		
-		data.add(registro);
-
-		results.put("results", data);
-
-		return Response.ok("Sucess").build();
 	}
 	
 	@GET
